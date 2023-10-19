eb526b2d40e12079a26d2df368d0476f4aeeb4ae
sksusha8853 <sksusha8853@gmail.com>
2023-10-19 23:40:45 +0530

    added model to remove background, to detect single object and to detect multiple objects

---
+++ FrameToObjects/main.py
@@ -0,0 +1,11 @@
+from removeBackground import removeBackground
+from cropSingleObject import cropSingleObject
+from cropMultipleObjects import cropMultipleObjects
+def main():
+    removeBackground()
+    cropSingleObject()
+    cropMultipleObjects()
+
+
+if __name__ == "__main__":
+    main()
